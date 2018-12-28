d83e3db8685912d0529f5ef572bfb58cf60d0e5b
MisterAtoZ <jonathan.luijsmans@student.uhasselt.be>
2018-12-28 16:12:11 +0100

    toevoegen van rebar3

---
+++ projectTagp/README.md
@@ -0,0 +1,9 @@
+projectTagp
+=====
+
+An OTP application
+
+Build
+-----
+
+    $ rebar3 compile
---
+++ projectTagp/src/projectTagp_sup.erl
@@ -0,0 +1,41 @@
+%%%-------------------------------------------------------------------
+%% @doc projectTagp top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(projectTagp_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: #{id => Id, start => {M, F, A}}
+%% Optional keys are restart, shutdown, type, modules.
+%% Before OTP 18 tuples must be used to specify a child. e.g.
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    observer:start(),
+	survivor:start(),
+    testModule2:startNPipes(5),
+    {ok, {{one_for_all, 0, 1}, []}}.
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
